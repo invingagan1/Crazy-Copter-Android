cab4233ded792ab8d6f3aa440d7abba3532aaa69
Gagan Deep Singh <hbti.gagan+github@gmail.com>
2018-06-17 16:12:46 +0530

    platform detection added.

--- web-code/js/utils/utils.js
+++ web-code/js/utils/utils.js
@@ -4,5 +4,8 @@ var utils = {
             var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
+    },
+    isMobileDevice: function(){
+        return (/Mobi|Android/i.test(navigator.userAgent));
     }
 }
